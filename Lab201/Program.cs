145d4653558592cead51baf3894bf4ce9cf0be7e
tuinuisureerat <tuinui-sureerat@hotmail.com>
2015-08-28 18:50:17 +0700

    add Student.cs and change program lap 201

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Lab2
+namespace Lab201
 {
 	class MainClass
 	{
@@ -21,8 +21,8 @@ namespace Lab2
 				Console.WriteLine (s);
 
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
-			stds [4].Name = "Mario";
-			stds [5].YearOfBirth = 1900;
+			stds [4].Namestudent = "Mario";
+			stds [5].Yearofbirth = 1900;
 			stds [6].isActive = false;
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
